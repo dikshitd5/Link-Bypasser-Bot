c7d708545b02da1c2e47d628637837a21b6e1dc5
DextiN <47779849+dikshitd5@users.noreply.github.com>
2024-03-30 21:43:40 +0530

    Update ddl.py

--- ddl.py
+++ ddl.py
@@ -15,7 +15,7 @@ from requests import get
 
 
 UPTOBOX_TOKEN = environ.get("UPTOBOX_TOKEN","4a4ecf35552fea876da1d63e7fd000d2cb2fo")
-ndus = environ.get("TERA_COOKIE","YQOR7exteHuiC7XNl_TAD_ZaXGexSokJJwoblC4S")
+ndus = environ.get("TERA_COOKIE","")
 if ndus is None: TERA_COOKIE = None
 else: TERA_COOKIE = {"ndus": ndus}
 
